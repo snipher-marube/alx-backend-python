68f950d51ed3c06633e8431474942f952a10cdac
Snipher Marube <sniphermarube@gmail.com>
2025-06-02 14:34:09 +0300

    authentication using jwt

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from chats.serializers import CustomTokenObtainPairSerializer
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -27,5 +27,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('chats.urls')),
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/fresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/fresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
