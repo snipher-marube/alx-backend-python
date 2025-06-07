4545186394c19da3b5f60084a4ba8d479ec2cf92
Snipher Marube <sniphermarube@gmail.com>
2025-06-07 20:34:34 +0300

    basic authentication class

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from rest_framework import viewsets, permissions, status, filters, serializers
+from rest_framework import viewsets, status, filters, serializers
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Conversation, Message
@@ -21,10 +21,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
     ordering = ['-updated_at']
 
     def get_queryset(self):
-        """
-        Return only conversations where the current user is a participant
-        """
-        return self.queryset.filter(participants=self.request.user)
+        """Return only conversations where the current user is a participant"""
+        return super().get_queryset().filter(participants=self.request.user)
 
     def get_serializer_class(self):
         if self.action == 'create':
@@ -33,18 +31,36 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         conversation = serializer.save()
-        if self.request.user not in conversation.participants.all():
-            conversation.participants.add(self.request.user)
+        conversation.participants.add(self.request.user)
 
-    @action(detail=True, methods=['get'])
+    @action(detail=True, methods=['get', 'put', 'patch', 'delete'])
     def messages(self, request, pk=None):
         conversation = self.get_object()
-        if request.user not in conversation.participants.all():
-            return Response(
-                {"detail": "Not a participant"},
-                status=status.HTTP_403_FORBIDDEN
-            )
         messages = conversation.messages.all().order_by('sent_at')
+        
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
+            message_id = request.data.get('id')
+            message = Message.objects.filter(
+                id=message_id,
+                conversation_id=conversation.id,
+                sender=request.user
+            ).first()
+            
+            if not message:
+                return Response(
+                    {"detail": "Message not found or not owned by user"},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            
+            if request.method == 'DELETE':
+                message.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            else:
+                serializer = MessageSerializer(message, data=request.data, partial=True)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(serializer.data)
+        
         serializer = MessageSerializer(messages, many=True)
         return Response(serializer.data)
 
@@ -58,7 +74,7 @@ class MessageViewSet(viewsets.ModelViewSet):
     ordering = ['-sent_at']
 
     def get_queryset(self):
-        return self.queryset.filter(
+        return super().get_queryset().filter(
             Q(conversation__participants=self.request.user) |
             Q(sender=self.request.user)
         ).distinct()
@@ -70,7 +86,6 @@ class MessageViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         conversation = serializer.validated_data['conversation']
-        if self.request.user not in conversation.participants.all():
+        if not conversation.participants.filter(id=self.request.user.id).exists():
             raise serializers.ValidationError("Not a participant")
         serializer.save(sender=self.request.user)
-        
