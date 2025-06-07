f466cd4daec1b0d7add5069388020a249c723591
Snipher Marube <sniphermarube@gmail.com>
2025-06-07 20:45:12 +0300

    basic authentication class

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,16 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
     page_size = 20  # Default page size
     page_size_query_param = 'page_size'  # Allow client to override
     max_page_size = 100  # Maximum limit
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,  # Total item count
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'page_size': self.get_page_size(self.request),
+            'results': data
+        })
