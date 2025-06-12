ee31610993c23845954f813190751aeacc53c01d
Snipher Marube <sniphermarube@gmail.com>
2025-06-12 14:15:16 +0300

    chat

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -12,12 +12,48 @@ class Message(models.Model):
     is_read = models.BooleanField(default=False)
     edited = models.BooleanField(default=False)
     last_edited = models.DateTimeField(null=True, blank=True)
+    parent_message = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='replies'
+    )
+    is_thread_starter = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Message from {self.sender} to {self.receiver}"
 
+    def save(self, *args, **kwargs):
+        # Automatically set is_thread_starter based on parent_message
+        if self.parent_message is None:
+            self.is_thread_starter = True
+        super().save(*args, **kwargs)
+
+    def get_thread(self):
+        """
+        Returns the entire thread of messages including all replies
+        """
+        messages = []
+        if self.is_thread_starter:
+            messages = Message.objects.filter(
+                models.Q(pk=self.pk) |
+                models.Q(parent_message=self)
+            ).select_related(
+                'sender',
+                'receiver',
+                'parent_message'
+            ).prefetch_related(
+                'replies'
+            ).order_by('timestamp')
+        return messages
+
     class Meta:
         ordering = ['-timestamp']
+        indexes = [
+            models.Index(fields=['parent_message']),
+            models.Index(fields=['is_thread_starter']),
+        ]
 
 class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
